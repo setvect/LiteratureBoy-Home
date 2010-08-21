ba97c92e79b1f1c7b40e4a9b9c22064c2fd1dbcf
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2010-08-21 12:24:07 +0000

    git-svn-id: http://literatureboy.googlecode.com/svn/trunk@72 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

--- source/test/test/MigrationTestCase.java
+++ source/test/test/MigrationTestCase.java
@@ -2,13 +2,21 @@ package test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.annotation.Resource;
 
+import org.hibernate.SQLQuery;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
 import org.junit.Test;
 import org.springframework.test.annotation.Rollback;
 
-import com.setvect.literatureboy.service.memo.MemoService;
+import com.setvect.literatureboy.service.board.BoardService;
+import com.setvect.literatureboy.vo.board.Board;
+import com.setvect.literatureboy.vo.board.BoardArticle;
+import com.setvect.literatureboy.vo.board.BoardAttachFile;
 
 /**
  * 마이그레이션 프로그램
@@ -17,7 +25,9 @@ import com.setvect.literatureboy.service.memo.MemoService;
  */
 public class MigrationTestCase extends TestSystem {
 	@Resource
-	private MemoService service;
+	private BoardService boardService;
+	@Resource
+	private SessionFactory sessionFactory;
 
 	/**
 	 * CRUD 테스트
@@ -27,7 +37,96 @@ public class MigrationTestCase extends TestSystem {
 	@Test
 	@Rollback(false)
 	public void migration() throws Exception {
+		Session session = sessionFactory.getCurrentSession();
+
+		SQLQuery s = session.createSQLQuery("delete from TBBD_BOARD_FILE");
+		s.executeUpdate();
+
+		s = session.createSQLQuery("delete from TBBC_BOARD_COMMENT	");
+		s.executeUpdate();
+
+		s = session.createSQLQuery("delete from TBBB_BOARD_ARTICLE");
+		s.executeUpdate();
+
+		s = session.createSQLQuery("delete from TBBA_BOARD_MANAGER");
+		s.executeUpdate();
+
 		Class.forName("org.gjt.mm.mysql.Driver");
 		Connection sourceConnecton = DriverManager.getConnection("jdbc:mysql://localhost:3306/setvect", "root", "7805");
+		Statement st = sourceConnecton.createStatement();
+		ResultSet rs = st.executeQuery("select * from TBBA_BOARD_MANAGER");
+		while (rs.next()) {
+			Board board = new Board();
+			board.setBoardCode(rs.getString("BOARD_CODE"));
+			board.setName(rs.getString("NAME"));
+			board.setUploadLimit(rs.getInt("FILESIZE_LM"));
+			board.setReplyF(rs.getString("REPLY_F").equals("Y"));
+			board.setCommentF(rs.getString("COMMENT_F").equals("Y"));
+			board.setAttachF(rs.getString("PDS_F").equals("Y"));
+			board.setEncodeF(rs.getString("ENCODE_F").equals("Y"));
+			board.setDeleteF(false);
+			boardService.createBoard(board);
+		}
+		rs.close();
+		st.close();
+
+		st = sourceConnecton.createStatement();
+		rs = st.executeQuery("select count(*) from TBBB_BOARD_ARTICLE");
+		rs.next();
+		int total = rs.getInt(1);
+
+		rs.close();
+		st.close();
+
+		st = sourceConnecton.createStatement();
+		rs = st.executeQuery("select * from TBBB_BOARD_ARTICLE");
+		int count = 0;
+		while (rs.next()) {
+
+			BoardArticle article = new BoardArticle();
+			article.setBoardCode(rs.getString("BOARD_CODE"));
+			article.setMemberId(rs.getString("MEMBER_ID"));
+			article.setIdx1(rs.getInt("IDX1"));
+			article.setIdx2(rs.getInt("IDX2"));
+			article.setIdx3(rs.getInt("IDX3"));
+			article.setDepthLevel(rs.getInt("DEPTH_LEV"));
+			article.setTitle(rs.getString("Title"));
+			article.setName(rs.getString("NAME"));
+			article.setEmail(rs.getString("EMAIL"));
+			article.setPasswd(rs.getString("PASSWD"));
+			article.setContent(rs.getString("CONTENT"));
+			article.setIp(rs.getString("IP"));
+			article.setHitCount(rs.getInt("HIT_COUNT"));
+			article.setEncodeF(rs.getString("ENCODE_F").equals("Y"));
+			article.setRegDate(rs.getDate("REG_DATE"));
+			article.setDeleteF(false);
+			boardService.createArticle(article);
+
+			int articleSeq = rs.getInt("ARTICLE_SEQ");
+
+			Statement stComment = sourceConnecton.createStatement();
+			ResultSet rsComment = stComment.executeQuery("select * from TBBD_BOARD_FILE where ARTICLE_SEQ = "
+					+ articleSeq + " order by ARTICLE_SEQ");
+			while (rsComment.next()) {
+				BoardAttachFile attach = new BoardAttachFile();
+				attach.setArticleSeq(article.getArticleSeq());
+				attach.setOriginalName(rsComment.getString("ORG_FILENAME"));
+				attach.setSaveName(rsComment.getString("SAV_FILENAME"));
+				attach.setSize(rsComment.getInt("FILE_SIZE"));
+				boardService.createAttachFile(attach);
+			}
+			rsComment.close();
+			stComment.close();
+
+			count++;
+			if (count % 10 == 0) {
+				System.out.println(count + " / " + total + " Running");
+			}
+		}
+		System.out.println(total + " Complete");
+		rs.close();
+		st.close();
+
+		sourceConnecton.close();
 	}
 }
