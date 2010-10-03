90b5079f24d823f1bcf9137d0ec0a3482c64c96c
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2010-10-03 07:18:58 +0000

    Complete - task : [기능] 트래백
    git-svn-id: http://literatureboy.googlecode.com/svn/trunk@123 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

---
+++ source/common/com/setvect/common/net/NetworkUtil.java
@@ -0,0 +1,26 @@
+package com.setvect.common.net;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @version $Id$
+ */
+public class NetworkUtil {
+	/**
+	 * URL을 받아 홈페이지 파일 경로 제거하고 홈페이지 주소만 리턴 (끝에 '/' 없음)
+	 * 
+	 * @param sb
+	 *            request.getRequestURL()
+	 * @return 순수 홈페이지 주소만
+	 */
+	public static String getHomepageUrl(HttpServletRequest req) {
+		StringBuffer rtn = new StringBuffer("http://");
+		rtn.append(req.getServerName());
+		// 80포트가 아닌 것은 포트 추가
+		if (req.getServerPort() != 80) {
+			rtn.append(":");
+			rtn.append(req.getServerPort());
+		}
+		return rtn.toString();
+	}
+}
