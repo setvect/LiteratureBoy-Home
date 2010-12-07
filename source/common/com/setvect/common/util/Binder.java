3237b7b97e86e431006c1d9e0ca0957277f12642
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2010-12-07 05:36:15 +0000

    Binder Update
    git-svn-id: http://literatureboy.googlecode.com/svn/trunk@144 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

--- source/common/com/setvect/common/util/Binder.java
+++ source/common/com/setvect/common/util/Binder.java
@@ -1,5 +1,12 @@
 package com.setvect.common.util;
 
+import java.beans.PropertyEditorSupport;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.ServletRequestBindingException;
@@ -22,8 +29,59 @@ public class Binder {
 	 * @throws ServletRequestBindingException
 	 */
 	public static void bind(HttpServletRequest request, Object object) throws ServletRequestBindingException {
+		bind(request, object, null);
+	}
+
+	/**
+	 * @param request
+	 * @param object
+	 * @param customEditor
+	 *            bind될 속성 클래스에 따른 request 방법
+	 * @throws ServletRequestBindingException
+	 */
+	public static void bind(HttpServletRequest request, Object object, Map<Class<?>, PropertyEditorSupport> customEditor)
+			throws ServletRequestBindingException {
 		ServletRequestDataBinder binder = new MultiFileSupportServletRequestDataBinder(object);
+
+		if (customEditor != null) {
+			Set<Class<?>> keys = customEditor.keySet();
+			for (Class<?> k : keys) {
+				binder.registerCustomEditor(k, customEditor.get(k));
+			}
+		}
+
 		binder.bind(request);
 		binder.closeNoCatch();
 	}
+
+	/**
+	 * @param dateFormat
+	 * @return 날짜 패턴 bind
+	 */
+	public static PropertyEditorSupport getDateFromatProperty(String dateFormat) {
+
+		return new DatePropertyEditorSupport(dateFormat) {
+			public void setAsText(String value) {
+				try {
+					setValue(new SimpleDateFormat(pattern).parse(value));
+				} catch (ParseException e) {
+					setValue(null);
+				}
+			}
+
+			public String getAsText() {
+				return new SimpleDateFormat(pattern).format((Date) getValue());
+			}
+		};
+
+	}
+
+	static class DatePropertyEditorSupport extends PropertyEditorSupport {
+		protected final String pattern;
+
+		public DatePropertyEditorSupport(String pattern) {
+			this.pattern = pattern;
+		}
+	}
+
 }
