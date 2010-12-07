de44c7bac92295a3c00ea5540520470a6cd2cc82
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2010-12-07 06:20:12 +0000

    Tree item
    git-svn-id: http://literatureboy.googlecode.com/svn/trunk@148 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

--- source/common/com/setvect/common/util/tree/TreeItem.java
+++ source/common/com/setvect/common/util/tree/TreeItem.java
@@ -4,21 +4,20 @@ package com.setvect.common.util.tree;
  * tree구조를 나타내기 위한 기본 인터페이스 항목 정보<br>
  * 트리 구조를 표현하기 위해서는 본 인터페이스를 구현 해야 됨 .
  */
-public interface TreeItem extends Comparable<Object> {
-
+public interface TreeItem<ID> extends Comparable<ID> {
 	/**
 	 * @return Returns the id.
 	 */
-	public Object getId();
+	public ID getId();
 
 	/**
 	 * @return Returns the parent.
 	 */
-	public Object getParent();
+	public ID getParent();
 
 	/**
 	 * @param level
 	 *            The level to set.
 	 */
-	public abstract void setLevel(int level);
+	public void setLevel(int level);
 }
