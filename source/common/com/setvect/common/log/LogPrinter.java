fe746b0f623a9d80fc9cb1f5c7de74b1fab4f09f
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2011-02-23 04:30:04 +0000

    git-svn-id: http://literatureboy.googlecode.com/svn/trunk@201 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

--- source/common/com/setvect/common/log/LogPrinter.java
+++ source/common/com/setvect/common/log/LogPrinter.java
@@ -1,6 +1,7 @@
 package com.setvect.common.log;
 
 import java.io.File;
+import java.net.URL;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
@@ -31,6 +32,21 @@ public class LogPrinter {
 		out = Logger.getLogger(LogPrinter.class);
 		// Use a PropertyConfigurator to initialize from a property file.
 		DOMConfigurator.configure(logFilePath.getPath());
+	}
+
+	/**
+	 * 로그 설정을 초기화 함
+	 * 
+	 * @param logFilePath
+	 *            로그 파일 설정 경로
+	 */
+	public static void init(URL logFilePath) {
+		if (init) {
+			throw new RuntimeException("configuration already setting");
+		}
+		out = Logger.getLogger(LogPrinter.class);
+
+		DOMConfigurator.configure(logFilePath);
 	}
 
 }
