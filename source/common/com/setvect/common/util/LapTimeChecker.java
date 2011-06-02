cf9ba8412c05844196b094f267ddd11e79f621aa
setvect@naver.com <setvect@naver.com@76c2b2f6-976e-2fa4-fefe-f2a6046cae85>
2011-06-02 05:27:37 +0000

    git-svn-id: http://literatureboy.googlecode.com/svn/trunk@282 76c2b2f6-976e-2fa4-fefe-f2a6046cae85

--- source/common/com/setvect/common/util/LapTimeChecker.java
+++ source/common/com/setvect/common/util/LapTimeChecker.java
@@ -1,6 +1,7 @@
-package com.setvect.common.util;
+	package com.setvect.common.util;
 
 import java.io.PrintStream;
+import java.text.DecimalFormat;
 
 /**
  * 시간을 체크 한다.<br>
@@ -9,6 +10,20 @@ import java.io.PrintStream;
  * @version $Id$
  */
 public class LapTimeChecker {
+
+	/** 1초를 밀리세컨드로 */
+	private static final int SEC_FOR_MS = 1000;
+	/** 1분을 밀리세컨드로 */
+	private static final int MIN_FOR_MS = SEC_FOR_MS * 60;
+	/** 1시간을 밀리세컨드로 */
+	private static final int HOUR_FOR_MS = MIN_FOR_MS * 60;
+	/** 하루를 밀리세컨드로 */
+	private static final int DAY_FOR_MS = HOUR_FOR_MS * 24;
+
+	private static final DecimalFormat NUMBER_FORMAT = new DecimalFormat("0000");
+	private static final DecimalFormat NUMBER_COMMA_FORMAT = new DecimalFormat(",###");
+	private static final DecimalFormat NUMBER_TIME_FORMAT = new DecimalFormat("00");
+
 	/** 체크 시작 시간 */
 	private long startTime;
 
@@ -20,6 +35,9 @@ public class LapTimeChecker {
 
 	/** 이름 */
 	private String name;
+
+	/** 전체 카운트 */
+	private int totalCount;
 
 	/**
 	 * 기본 생성자<br>
@@ -56,7 +74,70 @@ public class LapTimeChecker {
 	public void check(String message) {
 		checkCount++;
 		long cur = System.currentTimeMillis();
-		out.println(name + ", " + NumberFormat.getNumberString("0000", checkCount) + ", "
-				+ NumberFormat.getNumberString(",###", cur - startTime) + "ms, " + message);
+		out.println(name + ", " + NUMBER_FORMAT.format(checkCount) + ", " + NUMBER_COMMA_FORMAT.format(cur - startTime)
+				+ "ms, " + message);
 	}
+
+	/**
+	 * @param processCount
+	 *            처리된 건수
+	 * @return 예상 남은 시간을 ms 단위로 리턴
+	 */
+	public long getRemainTime(int processCount) {
+		long running = System.currentTimeMillis() - startTime;
+		return (running * totalCount / processCount) - running;
+	}
+
+	/**
+	 * @param processCount
+	 *            처리된 건수
+	 * @param pattern
+	 *            출력 패턴, 날짜 패턴
+	 * @return 예상 남은 시간을 정해진 포맷에 의해 문자열로 제공
+	 */
+	public String getRemainTimeFormat(int processCount) {
+		long time = getRemainTime(processCount);
+		return getTimeToDayFormat(time);
+	}
+
+	/**
+	 * @return 수행 시간을 날짜/시간 포맷으로
+	 */
+	public String getRunningTimeFormat() {
+		long runningTime = System.currentTimeMillis() - startTime;
+		return getTimeToDayFormat(runningTime);
+	}
+
+	private String getTimeToDayFormat(long time) {
+		long day = time / DAY_FOR_MS;
+		time = time % DAY_FOR_MS;
+
+		long hour = time / HOUR_FOR_MS;
+		time = time % HOUR_FOR_MS;
+
+		long min = time / MIN_FOR_MS;
+		time = time % MIN_FOR_MS;
+
+		long sec = time / SEC_FOR_MS;
+
+		StringBuffer rtnValue = new StringBuffer();
+		if (day != 0) {
+			rtnValue.append(NUMBER_TIME_FORMAT.format(day) + "day(s) ");
+		}
+		rtnValue.append(NUMBER_TIME_FORMAT.format(hour) + ":");
+		rtnValue.append(NUMBER_TIME_FORMAT.format(min) + ":");
+		rtnValue.append(NUMBER_TIME_FORMAT.format(sec));
+		return rtnValue.toString();
+	}
+
+	/**
+	 * 현재 진행 시간 대비 전체 카운트를 계산하여 총 예상 시간을 구함
+	 * 
+	 * @param totalCount
+	 *            총 작업 건수
+	 */
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
 }
